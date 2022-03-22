a1cad68c156054c9b7af50913431e12d5d3d204f
Miguel Traiano <mtraiano76@gmail.com>
2022-03-22 08:34:58 -0300

    Sequelize

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "coco1002",
+    "database": "movie_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
